b0f931edc4fe4ab559f6abe776c5d6a4981f8afe
Nwekeson Chibuzor <zaddychiboy@gmail.com>
2025-07-08 10:59:36 +0100

    Included WelcomeMessage component along with Header, MainContent, and Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
     <>
       <Header />
+      <WelcomeMessage />
       <MainContent />
       <Footer />
     </>
