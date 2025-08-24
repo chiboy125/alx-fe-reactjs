42b5043a945d646d55dd32e2e79a08253d155393
Nwekeson Chibuzor <zaddychiboy@gmail.com>
2025-08-24 08:16:44 +0100

    Fix: Import and render TodoList in App.jsx

---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+// src/components/TodoList.jsx
+import React from "react";
+
+function TodoList() {
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul>
+        <li>Sample Todo 1</li>
+        <li>Sample Todo 2</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
